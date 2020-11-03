26fa6b5b9da27ffb30ae1247b8753018af9ffdd5
Pablo Guzmán <peguzman@teco.com.ar>
2020-11-03 09:48:32 -0300

    service worker added

---
+++ next.config.js
@@ -0,0 +1,41 @@
+const withOffline = require('next-offline');
+
+const nextConfig = {
+    devIndicators: {
+        autoPrerender: false
+    },
+    workboxOpts: {
+        runtimeCaching: [
+            {
+                urlPattern: /.jpg$/,
+                handler: 'CacheFirst',
+                options: {
+                    cacheName: 'images'
+                }
+            },
+            {
+                urlPattern: /^https:\/\/yts.mx\/api\/v2\/list_movies/i,
+                handler: 'StaleWhileRevalidate',
+                options: {
+                    cacheName: 'api-list'
+                }
+            },
+            {
+                urlPattern: /^https:\/\/yts.mx\/api\/v2\/movie_details.json/i,
+                handler: 'StaleWhileRevalidate',
+                options: {
+                    cacheName: 'api-details'
+                }
+            },
+            {
+                urlPattern: /^https:\/\/yts.mx\/api\/v2\/movie_suggestions.json/i,
+                handler: 'StaleWhileRevalidate',
+                options: {
+                    cacheName: 'api-suggestions'
+                }
+            }
+        ]
+    }
+};
+
+module.exports = withOffline(nextConfig);
